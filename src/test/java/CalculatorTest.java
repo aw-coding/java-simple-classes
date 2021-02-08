ce0e06ec7f44fe7b90a509018f741e4d00b66a8d
Andrew Wood <andrewwood90@hotmail.com>
2021-02-08 15:50:46 +0000

    calculator tests finished

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -13,7 +13,22 @@ public class CalculatorTest {
     }
 
     @Test
-    public void AddTest() {
+    public void addTest() {
         assertEquals(10, calculator.add(8, 2));
+    }
+
+    @Test
+    public void subtractTest(){
+        assertEquals(14, calculator.subtract(20, 6));
+    }
+
+    @Test
+    public void multiplyTest(){
+        assertEquals(64, calculator.multiply(8, 8));
+    }
+
+    @Test
+    public void divideTest(){
+        assertEquals(3.5, calculator.divide(7, 2), 0.0);
     }
 }
