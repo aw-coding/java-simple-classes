24b0878ca84c668842da53cc43bdf2482220cfaf
Andrew Wood <andrewwood90@hotmail.com>
2021-02-08 15:29:09 +0000

    initial commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void AddTest() {
+        assertEquals(10, calculator.add(8, 2));
+    }
+}
